f29e2d895328465aef7a29f1831d76119fac5315
EYoung75 <eyoung75.ey@gmail.com>
2019-04-08 13:17:59 -0600

    added first action creator and implemented first reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
+import PostList from "./PostList"
 
 class App extends Component {
   render() {
     return (
       <div className="ui container">
-       
+        <PostList/>
       </div>
     );
   }
